22f77dcb935599d8794f1a96df8440aa301b3785
Matthew Maertz <45174542+matthewmaertz@users.noreply.github.com>
2020-04-24 12:45:58 -0400

    Add files via upload

---
+++ setup_new_computer.sh
@@ -0,0 +1,10 @@
+apt install update
+apt install upgrade -y
+apt install guake
+apt install git
+apt install snap
+apt install htop
+apt install thunderbird
+snap install spotify
+apt install fish
+chsh -s /bin/usr/fish
